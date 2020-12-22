1c3d22b13651a207d22dc3776d0aab4e09fab706
herlessalvarado <herlessalvarado@gmail.com>
2020-12-22 00:28:39 -0500

    cut from cube and clean code

--- Octree.h
+++ Octree.h
@@ -1,10 +1,6 @@
 #ifndef OCTREE_H
 #define OCTREE_H
 
-#define cimg_display  1
-#define cimg_use_png  1
-#define cimg_use_jpeg 1
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -14,24 +10,10 @@
 using namespace cimg_library;
 using namespace std;
 
-void Binarizar(CImg<float> & img, int umbral, int z_plane, char cube[][512][512]){
-    for(int i=0; i<img.width(); i++){
-        for(int j=0; j<img.height(); j++){
-            int r = img(i,j,0);
-            int g = img(i,j,1);
-            int b = img(i,j,2);
-            if ( (r+g+b)/3 > umbral)
-                cube[z_plane][i][j] = (char) 0;
-            else
-                cube[z_plane][i][j] = (char) 255;
-        }
-    }
-};
-
 class Octree {
 private:
     Node* root;
-	CImg<char> cuttedImage;
+	CImg<char> outputImg;
     int a,b,c,d;
 
     bool uniqueColor(int xi, int xf, int yi, int yf, int zi, int zf, char img[][512][512]) {
@@ -84,13 +66,13 @@ private:
 					for (int j = node->yi; j <= node->yf; ++j) {
 						if(a*i + b*j + c*k + d == 0) {
 							if(a != 0) {
-								cuttedImage(j,k) = node->color;
+								outputImg(j,k) = node->color;
 							}
 							else if(b != 0) {
-								cuttedImage(i,k) = node->color;
+								outputImg(i,k) = node->color;
 							}
 							else {
-								cuttedImage(i,j) = node->color;
+								outputImg(i,j) = node->color;
 							}
 						}
 					}
@@ -106,34 +88,29 @@ private:
 	}
 
 public:
-    Octree(){
-        string path = "data/paciente 1/1/Paciente1CC-27-10-1988- CT from 18-01-2011 S0 I";
-        static char cube[40][512][512];
-		for(int i = 0; i < 40; i++) {
-			string tomography = path + to_string(i) + ".BMP";
-			CImg<float> R(tomography.c_str());
-			Binarizar(R,128,i,cube);
-		}
-        insert(0, 511, 0, 511, 0, 39, cube, root);
-    }
+    Octree(){};
+
+	void insert(int xi, int xf, int yi, int yf, int zi, int zf, char img[][512][512]){
+		insert(xi, xf, yi, yf, zi, zf, img, root);
+	}
 
     void getCut(int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3){
-        int a1 = x2 - x1;
-        int b1 = y2 - y1;
-        int c1 = z2 - z1;
-        int a2 = x3 - x1;
-        int b2 = y3 - y1;
-        int c2 = z3 - z1;
-        a = b1 * c2 - b2 * c1;
-        b = a2 * c1 - a1 * c2;
-        c = a1 * b2 - b1 * a2;
-        d = (- a * x1 - b * y1 - c * z1);
-
-        cuttedImage.resize(512,512);
+		int a1 = x2 - x1;
+		int b1 = y2 - y1;
+		int c1 = z2 - z1;
+		int a2 = x3 - x1;
+		int b2 = y3 - y1;
+		int c2 = z3 - z1;
+		a = b1 * c2 - b2 * c1;
+		b = a2 * c1 - a1 * c2;
+		c = a1 * b2 - b1 * a2;
+		d = (- a * x1 - b * y1 - c * z1);
+		
+        outputImg.resize(512,512);
 
         getCut(root);
 
-        cuttedImage.display();
+        outputImg.display();
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,54 @@
+#define cimg_display  1
+#define cimg_use_png  1
+#define cimg_use_jpeg 1
+
+#include "CImg.h"
 #include "Octree.h"
+#include <string>
+
+using namespace cimg_library;
+using namespace std;
+
+void Binarizar(CImg<float> & img, int umbral, int z_plane, char cube[][512][512]){
+    for(int i=0; i<img.width(); i++){
+        for(int j=0; j<img.height(); j++){
+            int r = img(i,j,0);
+            int g = img(i,j,1);
+            int b = img(i,j,2);
+            if ( (r+g+b)/3 > umbral)
+                cube[z_plane][i][j] = (char) 0;
+            else
+                cube[z_plane][i][j] = (char) 255;
+        }
+    }
+};
+
+void getCutFromCube(int xi, int xf, int yi, int yf, int z, char img[][512][512]){
+    CImg<unsigned char> outputImg;
+    outputImg.resize(512,512);
+    for(int i = xi; i < xf; i++){
+        for(int j = yi; j < yf; j++){
+            outputImg(i,j) = img[z][i][j];
+        }
+    }
+    outputImg.display();
+}
 
 int main() {
+    string path = "data/paciente 1/1/Paciente1CC-27-10-1988- CT from 18-01-2011 S0 I";
+    static char cube[40][512][512];
+    for(int i = 0; i < 40; i++) {
+        string tomography = path + to_string(i) + ".BMP";
+        CImg<float> R(tomography.c_str());
+        Binarizar(R,128,i,cube);
+        R.display();
+    }
+    for(int z = 0; z < 40; z++) {
+        getCutFromCube(0,512,0,512,z,cube);
+    }
 	Octree octree;
+    octree.insert(0, 511, 0, 511, 0, 39, cube);
 	octree.getCut(0,0,0,255,0,0,0,255,0);
-    octree.getCut(255,255,0,255,255,0,0,0,0);
+    octree.getCut(0,0,0,0,0,0,0,0,0);
     return 0;
 }
